88c7de8f6d92e005225e3fac3039ca627d30ef64
HennaKristal <hennakristal@outlook.com>
2025-01-28 18:33:08 +0200

    Moved to a new repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# Auto Clicker
+
+Auto Clicker is an application which allows you to automate mouse clicks, create macros,
+create hotkeys for mouse clicks and automate keyboard inputs.
+
+
+## How to Use
+
+To open the application just run the executable (AutoClicker.exe).
+You may delete all other files, they are not required to run the executable.
---
+++ SourceCode/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+    public partial class AutoClickerForm
+    {
+        // Import necessary user32.dll methods for dragging functionality
+        [DllImport("user32.dll")] private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")] private static extern bool ReleaseCapture();
+
+        // Handle the MouseDown event for dragging the form
+        private void MoveHandler(MouseEventArgs args)
+        {
+            // Allows you to drag the application by holding down left mouse button
+            if (args.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        // Allows you to drag the application my holding down left mouse button
+        private void MacroManagerForm_MouseDown(object sender, MouseEventArgs args)
+        {
+            MoveHandler(args);
+        }
+
+        private void ApplicationTitle_MouseDown(object sender, MouseEventArgs args)
+        {
+            MoveHandler(args);
+        }
+    }
+}
